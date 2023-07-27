609ad892ffa8f4a2573b55d77dffa44c092481b8
Vadim Kaushan <admin@disasm.info>
2023-07-27 10:02:51 +0100

    Simple HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use tiny_http::{Response, Server, StatusCode};
+
 fn main() {
-    println!("Hello, world!");
+    let port = match env::var("PORT") {
+        Ok(p) => p.parse::<u16>().unwrap(),
+        Err(..) => 8000,
+    };
+    let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), port);
+    let server = Server::http(address).unwrap();
+
+    let response = Response::empty(StatusCode(200));
+    for req in server.incoming_requests() {
+        let _ = req.respond(response.clone());
+    }
 }
