ff0e0a0e101402df42f79717d846b4221ccbd12b
Vadim Kaushan <admin@disasm.info>
2023-08-05 12:52:48 +0300

    Graceful termination

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::sync::Arc;
 use tiny_http::{Response, Server, StatusCode};
 
 fn main() {
@@ -8,10 +9,19 @@ fn main() {
         Err(..) => 8000,
     };
     let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), port);
-    let server = Server::http(address).unwrap();
+    let server = Arc::new(Server::http(address).unwrap());
+    println!("Server started.");
+
+    let server_copy = server.clone();
+    ctrlc::set_handler(move || {
+        println!("Received termination signal.");
+        server_copy.unblock();
+    })
+    .expect("Error setting signal handler");
 
     let response = Response::empty(StatusCode(200));
     for req in server.incoming_requests() {
         let _ = req.respond(response.clone());
     }
+    println!("Stopped.");
 }
